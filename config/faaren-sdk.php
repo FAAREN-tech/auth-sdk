3cab57fd0b7b32fb3e04a2d991104fdc913cdd70
Fabian H <fabian@faaren.com>
2022-04-06 23:25:57 +0200

    removed token option from config file

--- config/faaren-sdk.php
+++ config/faaren-sdk.php
@@ -8,12 +8,6 @@ return [
             'production' => env('NS_ENDPOINT_PRODUCTION', 'https://services.faaren.com/notification/'),
             'staging' => env('NS_ENDPOINT_STAGING', 'https://services.staging.faaren.com/notification/'),
             'dev' => env('NS_ENDPOINT_DEV', 'notification:8080/')
-        ],
-
-        'tokens' => [
-            'production' => env('NS_TOKEN_PRODUCTION', 'nq6FjyM8HkUYX59mCbuwFwnDgduw54pJMTRdY8MxjC5UkNhshRK5WTDv6kxdsnRX'),
-            'staging' => env('NS_TOKEN_STAGING', 'zVHGJsQM4XL9CN8WqftvC6YWx2kgsbEnTeeXJrAUV77YgLFaXaWSwtV2W3Bn387y'),
-            'dev' => env('NS_TOKEN_DEV', 'ZQMQqW9DaS4Gs9wpcSEQ5xjm7nYaQCb9K6dYwjTMFkrJwRww4C2BV28TCH26fMCk')
         ]
     ]
 ];
